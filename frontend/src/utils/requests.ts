dd8cbbafa4cbfe0f0bd629c977c7c8266ae94c3d
henriquelins <henriquelins@msn.com>
2021-05-08 21:52:55 -0300

    env_heroku_or_local

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,4 @@
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 //export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://sds3-henriquelins.herokuapp.com';
+//yaexport const BASE_URL = 'https://sds3-henriquelins.herokuapp.com';
+export const BASE_URL = 'https://sds3-henriquelins.herokuapp.com' ?? 'http://localhost:8080';
